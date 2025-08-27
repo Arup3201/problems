e2a50b6d57c66d08e4005fb6870de7ce154ff36a
Arup3201 <arupjana7365@gmail.com>
2025-08-27 19:24:35 +0530

    update: leetcode move zeros solved

--- leetcode/leetcode.go
+++ leetcode/leetcode.go
@@ -350,3 +350,21 @@ func RemoveElement(nums []int, val int) int {
 
 	return k
 }
+
+/*
+LC 283 - Move zeros
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+*/
+
+func MoveZeros(nums []int) {
+	k := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != 0 {
+			nums[k], nums[j] = nums[j], nums[k]
+			k++
+		}
+	}
+}
