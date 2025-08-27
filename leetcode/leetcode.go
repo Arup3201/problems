eb85654c55757a9cffbded99e9263c78b8ae329d
Arup3201 <arupjana7365@gmail.com>
2025-08-27 19:18:42 +0530

    update: leetcode remove element solved

--- leetcode/leetcode.go
+++ leetcode/leetcode.go
@@ -327,3 +327,26 @@ func RemoveDuplicates(nums []int) int {
 
 	return k
 }
+
+/*
+LC 27 - Remove element
+
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+*/
+
+func RemoveElement(nums []int, val int) int {
+	k := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != val {
+			nums[k], nums[j] = nums[j], nums[k]
+			k++
+		}
+	}
+
+	return k
+}
